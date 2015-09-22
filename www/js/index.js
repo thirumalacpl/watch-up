a1763a683291a796da7b47c5dbf26f1e9c62c127
kalyan <kalyan@staging.eimpressive.com>
2015-09-22 10:19:41 +0530

    watchup37

--- www/js/index.js
+++ www/js/index.js
@@ -1,5 +1,5 @@
-$(document).on('pagecreate', '#pageone', function(){ 
-    //alert("hidash");
+$(document).on('pageshow', '#pageone', function(){ 
+    //alert("hidash"); 
     if(sessionStorage.getItem("logged_in")=="1"){
           $.mobile.changePage($('#dashboard'), { transition: "none", changeHash: true, reverse: false }); 
     }
